a30edef8cc83d82d5908a4626e75b5efe7eb974f
Belkys-Fernandez <belkysfernandez0@gmail.con>
2021-11-09 18:36:15 -0300

    usuario2

--- app/Entidades/Usuarios.php
+++ app/Entidades/Usuarios.php
@@ -1,6 +1,6 @@
 <?php
 
-    class usuario{
+    class Usuarios{
 
         public $NombreUsuario;
         public $Email;
@@ -30,28 +30,28 @@
             $accesoDatos=Acceso_datos::obtenerConexionBD();
             $consulta=$accesoDatos->prepararConsulta("SELECT * FROM usuario" );
             $consulta->execute();
-            return $consulta->fetchAll(PDO::FETCH_CLASS,'usuario'); 
+            return $consulta->fetchAll(PDO::FETCH_CLASS,'Usuarios'); 
         }
         
         public function buscarUsuarioPorId($nombreusuario){
             $accesoDatos=Acceso_datos::obtenerConexionBD();
             $consulta=$accesoDatos->prepararConsulta("SELECT * FROM usuario WHERE nombreusuario= $nombreusuario"  );
             $consulta->execute();
-            return $consulta->fetchAll(PDO::FETCH_CLASS,'usuario'); 
+            return $consulta->fetchAll(PDO::FETCH_CLASS,'Usuarios'); 
         }
         
         public function buscarDescripcionUsuario($nombreusuario){
             $accesoDatos=Acceso_datos::obtenerConexionBD();
             $consulta=$accesoDatos->prepararConsulta("SELECT contra FROM usuario WHERE nombreusuario= $nombreusuario "  );
             $consulta->execute();
-            return $consulta->fetchAll(PDO::FETCH_CLASS,'usuario'); 
+            return $consulta->fetchAll(PDO::FETCH_CLASS,'Usuarios'); 
         }
         
         public function buscarEliminar($nombreusuario){
             $accesoDatos=Acceso_datos::obtenerConexionBD();
              $consulta=$accesoDatos->prepararConsulta("DELETE FROM usuario WHERE nombreusuario= $nombreusuario "  );
             $consulta->execute();
-            return $consulta->fetchAll(PDO::FETCH_CLASS,'usuario'); 
+            return $consulta->fetchAll(PDO::FETCH_CLASS,'Usuarios'); 
         }
         
   
